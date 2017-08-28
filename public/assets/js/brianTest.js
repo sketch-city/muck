6de470e6ffbfe942def474db0ee5c2daeea4ec55
Brian <brianallenmarketing@gmail.com>
2017-08-28 11:01:56 -0700

    adding brian test page 3nd try

--- public/assets/js/brianTest.js
+++ public/assets/js/brianTest.js
@@ -1,15 +1,22 @@
-$(function() {
-
-    Parse.$ = jQuery;
-
+function helloMyParse()
+{
+    alert("attempting save");
     // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("texasRescue"); // Your App Name
+    Parse.initialize("cardforgegame"); // Your App Name
     Parse.serverURL = 'https://cardforge.herokuapp.com/parse'; // Your Server URL
+    //parse.useMasterKey = true;
 
     var TestObject = Parse.Object.extend("TestObject");
     var testObject = new TestObject();
-    testObject.save({foo: "barian test here now"}).then(function(object) {
-      alert("yay! it worked");
-    });
+    testObject.set("whatev",: "whatwwhat");
 
+    testObject.save(null, {
+    success: function(testObject) {
+      alert('New object created with objectId: ' + testObject.id);
+    },
+    error: function(testObject, error) {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and message.
+      alert('Failed to create new object, with error code: ' + error.message);
+    }
 });
