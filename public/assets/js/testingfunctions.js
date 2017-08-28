452f08019564272754c13fdfda89e19098abcebd
Brian <brianallenmarketing@gmail.com>
2017-08-28 12:46:43 -0700

    adding brian test page

--- public/assets/js/testingfunctions.js
+++ public/assets/js/testingfunctions.js
@@ -22,3 +22,30 @@ function helloMyParse()
 })
 
 };
+
+
+function createNewGPSMarker (name, description, positionData)
+{
+  alert("attempting save gps marker");
+  // Replace this line with the one on your Quickstart Guide Page
+  Parse.initialize("cardforgegame","brian"); // Your App Name
+  Parse.serverURL = 'https://cardforge.herokuapp.com/parse'; // Your Server URL
+  Parse.useMasterKey = true;
+
+  var TestObject = Parse.Object.extend("GPSMarkerObject");
+  var testObject = new TestObject();
+
+  testObject.set("name", name);
+  testObject.set("description",description);
+  testObject.set("positionData",positionData);
+  testObject.save(null, {
+  success: function(testObject) {
+    alert('New object created with objectId: ' + testObject.id);
+  },
+  error: function(testObject, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
+})
+}
