048ce6630c00738d9f63998c8c7ec75a53a08497
Brian <brianallenmarketing@gmail.com>
2017-08-29 01:21:24 -0700

    adding brian test page

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/alpha1', function(req, res) {
 
 var port = process.env.PORT || 1337;
 
-var httpsServer = require('https').createServer(options,app);
+var httpsServer = require('https').createServer(app);
 
 httpsServer.listen(1337, function() {
     console.log('parse-server-example running on port 1337.');
