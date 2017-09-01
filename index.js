157d2f2d3fe031ea3672379a815c984e60d625a7
antweer <trajwani@gmail.com>
2017-08-31 20:30:28 -0500

    map only

--- index.js
+++ index.js
@@ -51,6 +51,11 @@ app.get('/muckmap', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/mucked.html'));
 });
 
+// Texas Muck Map Only
+app.get('/muckmap/map', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/muckedMapOnly.html'));
+});
+
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
